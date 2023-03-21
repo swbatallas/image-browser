acd4c76ca6ba4f248dd6770ccd362f5cbec06ccb
Ulyssbat <stewartbatallasminda@gmail.com>
2023-03-21 21:53:43 +0100

    Añade html lint y eslint para typescript

--- src/fetchImagesAPI.ts
+++ src/fetchImagesAPI.ts
@@ -1,14 +1,43 @@
 export {fetchImagesFromAPI}
+export interface Photo {
+  id: number;
+  width: number;
+  height: number;
+  url: string;
+  photographer: string;
+  photographer_url: string;
+  photographer_id: string;
+  avg_color: string;
+  src: {
+  original: string;
+  large2x: string;
+  large: string;
+  medium: string;
+  small: string;
+  portrait: string;
+  landscape: string;
+  tiny: string;
+  };
+  }
+
+  export interface PhotoSearchAPIResult {
+    total_results: number;
+    page: number;
+    per_page: number;
+    photos: Photo[];
+    next_page: string;
+    }
 
 const PEXELS_API_KEY =
   "8L0s9HK3ypSMQ2i6l9sQqZobs1n02D39LeFBBKYQ6gaLrZ5Hgr3O5jJp";
-async function fetchImagesFromAPI(searchTerm: string, perPage: number) {
+
+async function fetchImagesFromAPI(searchTerm: string, perPage: number) :  Promise<PhotoSearchAPIResult> {
   const url = `https://api.pexels.com/v1/search?query=${searchTerm}&per_page=${perPage}`;
   const result = await fetch(url, {
     headers: {
-      Autorizhation: PEXELS_API_KEY,
+      Authorization: PEXELS_API_KEY,
     },
   });
-  const json = await result.json();
+  const json = await result.json() as PhotoSearchAPIResult;
   return json;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
-import './style.css'
-import {fetchImagesFromAPI} from './fetchImagesAPI'
- 
-fetchImagesFromAPI()
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import "./style.css";
+import { fetchImagesFromAPI } from "./fetchImagesAPI";
+import { render, html } from "lit-html";
+
+fetchImagesFromAPI("dogs", 5).then((data) => {
+  const htmlToRender = html`
+    <h1>Results for 'dogs'</h1>
+    <ul>
+      ${data.photos.map((photo) => {
+        return html`<li><img src=${photo.src.small} /></li>`;
+      })}
+    </ul>
+  `;
+  const div = document.getElementById("app");
+  if (!div) {
+    throw new Error("could not find app div");
+  }
+  render(htmlToRender, div);
+});
